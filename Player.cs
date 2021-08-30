07e7612ea5b2bdeee2afd433e96d2cd0ea6be6ed
emre0altan <emrealtann77@gmail.com>
2021-08-31 00:03:34 +0300

    initial

---
+++ Player.cs
@@ -0,0 +1,72 @@
+﻿namespace TennisTournament {
+
+    enum HandType { LEFT, RIGHT}
+
+    struct Skills {
+        public int clay;
+        public int grass;
+        public int hard;
+    }
+
+    struct PlayerResult {
+        public int order;
+        public int player_id;
+        public int gained_experience;
+        public int total_experience;
+    }
+
+    class Player {
+
+        public int id  { get; set; }
+        public int experience  { get; set; }
+        public HandType hand { get; set; }
+        public Skills skills;
+
+        int gainedExperience;
+
+        PlayerResult playerResult;
+
+        Player() { }
+
+        /// <summary>
+        /// Returns skill amount of the surface
+        /// </summary>
+        public int GetSurfaceSkill(SurfaceType _surface) {
+            if (_surface == SurfaceType.CLAY) return skills.clay;
+            else if (_surface == SurfaceType.GRASS) return skills.grass;
+            else return skills.hard;
+        }
+
+        /// <summary>
+        /// Returns gained experience during tournaments
+        /// </summary>
+        public int GetGainedExperience() {
+            return gainedExperience;
+        }
+
+        /// <summary>
+        /// Returns result of the player after the all tournaments
+        /// </summary>
+        public PlayerResult GetPlayerResult() {
+            return playerResult;
+        }
+
+        /// <summary>
+        /// Adds new experience
+        /// </summary>
+        public void AddGainedExperience(int _amount) {
+            experience += _amount;
+            gainedExperience += _amount;
+        }
+
+        /// <summary>
+        /// Sets values of the result
+        /// </summary>
+        public void SetResults(int _order) {
+            playerResult.order = _order;
+            playerResult.player_id = id;
+            playerResult.gained_experience = gainedExperience;
+            playerResult.total_experience = experience;
+        }
+    }
+}
---
+++ TournamentSimulation.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace TennisTournament {
+    struct FinalResults {
+        public List<PlayerResult> results;
+    }
+
+    class TournamentSimulation {
+
+        /// <summary>
+        /// Reads a json file and returns it as a JOBject
+        /// </summary>
+        static JObject ReadJsonFile(string _path) {
+            using (StreamReader file = File.OpenText(_path))
+            using (JsonTextReader reader = new JsonTextReader(file)) {
+                return (JObject)JToken.ReadFrom(reader);
+            }
+        }
+
+        /// <summary>
+        /// Deserializing the players from json
+        /// </summary>
+        static void GetJsonPlayers(JToken _jTokens, List<Player> _players) {
+            foreach (JToken jToken in _jTokens) {
+                _players.Add(JsonConvert.DeserializeObject<Player>(jToken.ToString(Formatting.None)));
+            }
+        }
+
+        /// <summary>
+        /// Deserializing the tournaments from json
+        /// </summary>
+        static void GetJsonTournaments(JToken _jTokens, List<Tournament> _tournaments) {
+            foreach (JToken jToken in _jTokens) {
+                _tournaments.Add(JsonConvert.DeserializeObject<Tournament>(jToken.ToString(Formatting.None)));
+            }
+        }
+
+        /// <summary>
+        /// Sorts players related to gained experience, if it is equal then total experience
+        /// </summary>
+        static void SortPlayers(List<Player> _players) {
+            _players.Sort((p1, p2) => {
+                int result = p2.GetGainedExperience().CompareTo(p1.GetGainedExperience());
+                if (result == 0)
+                    result = p2.experience.CompareTo(p1.experience);
+                return result;
+            });
+        }
+
+        /// <summary>
+        /// Sets player results with sort order
+        /// </summary>
+        static void SetPlayerResults(List<Player> _players) {
+            for(int i = 0; i < _players.Count; i++) {
+                _players[i].SetResults(i+1);
+            }
+        }
+
+        /// <summary>
+        /// Serializes the FinalResults to the json file 
+        /// </summary>
+        static void WriteJsonResults(FinalResults _finalResults, string _path) {
+            JsonSerializer serializer = new JsonSerializer();
+            serializer.Formatting = Formatting.Indented;
+
+            using (StreamWriter sw = new StreamWriter(_path))
+            using (JsonWriter writer = new JsonTextWriter(sw)) {
+
+                serializer.Serialize(writer, _finalResults);
+            }
+        }
+
+
+        static void Main(string[] args) {
+            string TournamentInputPath = "C:\\Users\\emre_\\Desktop\\TennisTournament\\input.json";
+            string TournamentOutputPath = "C:\\Users\\emre_\\Desktop\\TennisTournament\\output.json";
+
+            JObject inputData = ReadJsonFile(TournamentInputPath);
+
+            List<Player> players = new List<Player>();
+            GetJsonPlayers(inputData["players"], players);
+
+            List<Tournament> tournaments = new List<Tournament>();
+            GetJsonTournaments(inputData["tournaments"], tournaments);
+
+            foreach (Tournament tournament in tournaments)
+                tournament.MakeTournament(players);
+
+            SortPlayers(players);
+            SetPlayerResults(players);
+
+            FinalResults finalResults = new FinalResults();
+            finalResults.results = players.Select(o => o.GetPlayerResult()).ToList();
+            WriteJsonResults(finalResults, TournamentOutputPath);
+        }
+    }
+}
